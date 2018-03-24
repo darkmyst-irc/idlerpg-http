fe68c855b3d0eda709fa05b2ddcc46bec29f6020
pinkgothic <pinkgothic@gmail.com>
2018-03-24 19:54:24 +0100

    Enables player profile to show an unbounded amount of modifiers

--- controllers/PlayerController.php
+++ controllers/PlayerController.php
@@ -5,11 +5,16 @@ class PlayerController extends Controller
 
     public function characterAction($character)
     {
+        $modifiersToShow = 5;
+        if (array_key_exists('all-modifiers', $_GET)) {
+            $modifiersToShow = Controller::INFINITE_MODIFIERS;
+        }
+
         $view = new View('player-profile');
         $view->render(
             array(
                 'player'    => $this->getPlayer($character),
-                'modifiers' => $this->getModifiers($character),
+                'modifiers' => $this->getModifiers($character, $modifiersToShow),
             )
         );
     }
